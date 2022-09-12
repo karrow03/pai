ffc793a9051fba73cc2a2aacdc04f8925b020af1
karrow03 <karrow03@gmail.com>
2022-09-12 12:49:38 +0200

    pierwsze cos

---
+++ Nowy folder/Nowy dokument tekstowy.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+
+    <title> Hello world </title>
+    <meta charset="UTF-8">
+
+</head>
+
+
+<body>
+
+        <h3> Hello world </h3>
+
+
+
+            <?php
+
+                echo "Hello World" . "<br>" . "My counter 1" . "<br>" . "Hello friend";
+
+
+
+            ?>
+
+
+</body>
+
+</html>
+
