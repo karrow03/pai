c71583030a18a3c2fd659dcdf59cf5eebc41c622
karrow03 <karrow03@gmail.com>
2022-09-21 11:39:29 +0200

    procenty i continue

---
+++ procenty/edfwfe.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+</head>
+
+<body>
+
+
+<?php
+
+
+/* $procent = 65;
+
+if ($procent <=39) {
+    echo "Dostales 1";
+}
+elseif ($procent >=40 && $procent <=54) {
+    echo "Dostales 2";
+}
+elseif ($procent >=55 && $procent <=69) {
+    echo "Dostales 3";
+}
+elseif ($procent >=70 && $procent <=84) {
+    echo "Dostales 4";
+}
+elseif ($procent >=85 && $procent <=94) {
+    echo "Dostales 5";
+}
+elseif ($procent >=95 && $procent <=100) {
+    echo "Dostales 6";
+}
+
+*/
+
+
+
+
+
+
+for($i=-3; $i<=3; $i++) {
+    
+
+    if($i==0) continue;
+    echo $i . "<br>";
+}
+?>
+</body>
+</html>
---
+++ procenty/rotjgeroiugh.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+</head>
+
+<body>
+
+
+<?php
+
+
+$procent = 65;
+
+if ($procent <=39) {
+    echo "Dostales 1";
+}
+elseif ($prcoent >=40 && $procnet <=54) {
+    echo "Dostales 2";
+}
+elseif ($prcoent >=55 && $procnet <=69) {
+    echo "Dostales 3";
+}
+elseif ($prcoent >=70 && $procnet <=84) {
+    echo "Dostales 4";
+}
+elseif ($prcoent >=85 && $procnet <=94) {
+    echo "Dostales 5";
+}
+elseif ($prcoent >=95 && $procnet <=100) {
+    echo "Dostales 6";
+}
+
+
+?>
+</body>
+</html>
